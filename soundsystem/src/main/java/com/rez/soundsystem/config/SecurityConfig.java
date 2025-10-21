b2cfcf61dc3190492aeb2c877bb228e2da03e367
rysamn <fahrysaefulanwar25@gmail.com>
2025-10-21 15:47:20 +0700

    fix security

--- soundsystem/src/main/java/com/rez/soundsystem/config/SecurityConfig.java
+++ soundsystem/src/main/java/com/rez/soundsystem/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.rez.soundsystem.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity // <-- TAMBAHKAN ANOTASI INI
 public class SecurityConfig {
 
     @Autowired
