aec4c383bb2930e481daa06a4ca66d3df1b87fdf
Rasippp <rashifgibran@gmail.com>
2025-11-09 14:03:36 +0700

    fix merge konflik

--- soundsystem/src/main/java/com/rez/soundsystem/controller/InventoriController.java
+++ soundsystem/src/main/java/com/rez/soundsystem/controller/InventoriController.java
@@ -12,7 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+//rasip was here
 @RestController
 @RequestMapping("/api/inventori")
 @CrossOrigin(origins = "*")
